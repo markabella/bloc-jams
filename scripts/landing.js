429dec6012b5dd173926f71442b74991b9f30c6b
Mark Abella <mark.abella@gmail.com>
2017-05-20 17:09:05 -0700

    Assignment 9 Scripting completed

--- scripts/landing.js
+++ scripts/landing.js
@@ -1,24 +1,22 @@
 var pointsArray = document.getElementsByClassName('point');
 
+var revealPoint = function(point) {
+    //for (var i = 0; i < points.length; i++) {
+      point.style.opacity = 1;
+      point.style.transform = "scaleX(1) translateY(0)";
+      point.style.msTransform = "scaleX(1) translateY(0)";
+      point.style.WebkitTransform = "scaleX(1) translateY(0)";
+};
+
 var animatePoints = function(points) {
-
-  var revealPoint = function() {
-    for (var i=0;i<points.length;i++){
-      points[i].style.opacity = 1;
-      points[i].style.transform = "scaleX(1) translateY(0)";
-      points[i].style.msTransform = "scaleX(1) translateY(0)";
-      points[i].style.WebkitTransform = "scaleX(1) translateY(0)";
-    }
-  }
-
-  revealPoint();
+  forEach(points, revealPoint);
 };
 
 window.onload = function() {
   // Automatically animate the points on a tall screen where scrolling can't trigger the animation
-   if (window.innerHeight > 950) {
-       animatePoints(pointsArray);
-   }
+  if (window.innerHeight > 950) {
+    animatePoints(pointsArray);
+  }
 
   var sellingPoints = document.getElementsByClassName('selling-points')[0];
   var scrollDistance = sellingPoints.getBoundingClientRect().top - window.innerHeight + 200;
---
+++ scripts/utilities.js
@@ -0,0 +1,5 @@
+function forEach(ara, callback) {
+  for (var i = 0; i < ara.length; i++) {
+    callback(ara[i]);
+  }
+}
